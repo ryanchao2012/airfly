168a598ef41480ef1e8a356ece8a55d9f8f82a51
ryanchao2012 <ryanchao2012@gmail.com>
2021-08-05 03:08:55 +0800

    add: utils

--- src/airfly/utils.py
+++ src/airfly/utils.py
@@ -1,3 +1,6 @@
+import pathlib
+import subprocess as sp
+import tempfile
 from types import FunctionType, ModuleType
 from typing import Union
 
@@ -28,3 +31,43 @@ def qualname(obj: Union[FunctionType, ModuleType, type], level: int = -1) -> str
         name = ""
 
     return ".".join(name.split(".")[-level:]) if level > 0 else name
+
+
+def blacking(source_code: str):
+
+    with tempfile.NamedTemporaryFile("w", delete=False) as f:
+        f.write(source_code)
+        fname = f.name
+
+    p = sp.Popen(f"cat {fname}".split(), stdout=sp.PIPE)
+
+    out = sp.check_output("black -q -".split(), stdin=p.stdout)
+
+    p.wait()
+
+    try:
+        pathlib.Path(fname).unlink()
+    except FileNotFoundError:
+        pass
+
+    return out.decode()
+
+
+def isorting(source_code: str):
+
+    with tempfile.NamedTemporaryFile("w", delete=False) as f:
+        f.write(source_code)
+        fname = f.name
+
+    p = sp.Popen(f"cat {fname}".split(), stdout=sp.PIPE)
+
+    out = sp.check_output("isort -q -".split(), stdin=p.stdout)
+
+    p.wait()
+
+    try:
+        pathlib.Path(fname).unlink()
+    except FileNotFoundError:
+        pass
+
+    return out.decode()
